f3ca2b3dd94171565745f05f44779eb2b7d7b6e6
omar <omar@gmail.com>
2022-12-09 15:21:34 +0200

    create appbar

--- lib/screens/home_scree.dart
+++ lib/screens/home_scree.dart
@@ -7,6 +7,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
