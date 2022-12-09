c1480411d97af0e9c1c4c98947c5286d6dc42f0f
AmiraAdel28 <amira.adel282001@gmail.com>
2022-12-09 15:18:57 +0200

    create appbar and body

--- lib/screens/home_scree.dart
+++ lib/screens/home_scree.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,6 +6,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
   }
 }
